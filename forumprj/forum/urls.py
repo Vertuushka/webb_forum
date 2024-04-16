147a7e6a852c5a6b171a7af9bf206d6b4c5916b2
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-04-16 09:59:47 +0200

    fixed Alex's code

--- forumprj/forum/urls.py
+++ forumprj/forum/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:username>.<int:id>', views.profile_view, name='profile_view'),
-    path('<str:username>.<int:id>/edit', views.profile_edit, name="profile_edit"),
+    path('<int:id>', views.profile_view, name='profile_view'),
+    path('<int:id>/edit', views.profile_edit, name="profile_edit"),
 ]
--- forumprj/forum/views.py
+++ forumprj/forum/views.py
@@ -1,38 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm
-from . forms import UpdateUserInfo
+from . forms import *
 
 # Create your views here.
 
-def profile_view(request, username, id):
+def profile_view(request, id):
     # All info from DB about user
-    user = User.objects.get(username = username)
-    #TODO profile picture
-    #TODO role (ex. designer)
-    #TODO stuff member name
-    #TODO forum/first name
-    # userFirstName = user.first_name
-    #TODO forum title
-    # userLastName = user.last_name
-    #TODO email
-    # userEmail = user.email
-    #TODO data joined
-    # userDate = user.date_joined
-
+    user = User.objects.get(id=id)
     dictionary = {
-        "user": user
+        "user": user,
+        "account_id": id
     }
-    # print(user)
     return render(request, "profile.html", dictionary)
 
-def profile_edit(request, username, id):
+def profile_edit(request, id):
     if request.method == "POST":
         # built-in form in django to update user information 
         form = UpdateUserInfo(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect("profile_view", username, id)
+            return redirect("profile_view", id)
     else:
         form = UpdateUserInfo(instance=request.user)
 
