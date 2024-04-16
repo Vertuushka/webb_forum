09c21c7649a1ddf3b137ae393fbf23d58f6842cc
ErikasJanusauskas <erikas.privat3043@gmail.com>
2024-04-16 10:52:51 +0200

    base app + basic css setup

--- forumprj/account/forms.py
+++ forumprj/account/forms.py
@@ -1,11 +1,30 @@
+# from django.contrib.auth.forms import UserCreationForm
+
+# # Customize a UserCreationForm - remove unused texts
+# class SignupUserForm(UserCreationForm):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['password1'].help_text = None
+#         self.fields['password2'].help_text = None
+#     class Meta(UserCreationForm.Meta):
+#         fields = ['username', 'password1', 'password2', 'email']
+#         help_texts = {'username': None, 'email': None}
+
 from django.contrib.auth.forms import UserCreationForm
-      
-# Customize a UserCreationForm - remove unused texts
+
+# Customize a UserCreationForm - use placeholders and hide labels
 class SignupUserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['password1'].help_text = None
-        self.fields['password2'].help_text = None
+        # Set placeholders and remove labels
+        for fieldname in ['username', 'password1', 'password2', 'email']:
+            self.fields[fieldname].widget.attrs['placeholder'] = fieldname.capitalize()
+            self.fields[fieldname].label = False
+            self.fields[fieldname].help_text = None
+
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2', 'email']
-        help_texts = {'username': None, 'email': None}
+        help_texts = {
+            'username': None,
+            'email': None
+        }
--- forumprj/account/urls.py
+++ forumprj/account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login_user, name='login_user'),
-    path('logout', views.logout_user, name='logout_user'),
-    path('signup', views.create_user, name='create_user')
+    path('login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
+    path('signup/', views.create_user, name='create_user')
 ]
