b58ec14524653970395fd4b77a6f8b0190b4dac5
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-04-18 11:35:47 +0200

    context processor for menu (forum name + avatar)

---
+++ forumprj/base/context_processors.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from forum.models import Profile
+
+def menuProcessor(request):
+    if request.user.is_authenticated:
+        user = request.user
+        profile = Profile.objects.get(user=user)
+        if profile.profile_picture:
+            avatar_path = settings.MEDIA_URL + profile.profile_picture
+        else:
+            avatar_path = None
+        return {'avatar_path': avatar_path, 'currentUser':user}
+    return {'avatar_path': None, "currentUser":None}
