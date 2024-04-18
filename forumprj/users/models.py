67659356295e07c00992d1ab816d84ebfbc4bd01
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-04-18 20:46:23 +0200

    profile db issue fix
    Auto-adding new users to Profile db with empty avatar path to prevent query not exists error when trying to reach other pages on new account

--- forumprj/users/models.py
+++ forumprj/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.CharField(max_length=225, null=True, blank=True, default=None)
     info = models.CharField(max_length=200, null=True, blank=True, default=None)
 
