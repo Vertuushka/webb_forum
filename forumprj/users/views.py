8dcbf54ab2ab42fc13bdb1b6e7a8b73e70f8a452
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-04-19 18:35:59 +0200

    profile pictures uploading

--- forumprj/users/views.py
+++ forumprj/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+import os
 from . forms import *
 from . models import *
 
@@ -21,6 +23,19 @@ def profile_edit(request, id):
         # built-in form in django to update user information 
         form = UpdateUserInfo(request.POST, instance=request.user)
         if form.is_valid():
+            image_file = request.FILES.get('image')
+            if image_file:
+                eUser = User.objects.get(id=id)
+                file_extension = os.path.splitext(image_file.name)[1]
+                new_filename = slugify(f'{eUser.username}_avatar') + file_extension
+                image_file.name = new_filename
+                file_path = os.path.join(settings.MEDIA_ROOT, new_filename)
+                e_user = Profile.objects.get(user=eUser)
+                e_user.profile_picture = image_file.name
+                e_user.save()
+                with open(file_path, 'wb+') as destination:
+                    for chunk in image_file.chunks():
+                        destination.write(chunk)
             form.save()
             return redirect("profile_view", id)
     else:
