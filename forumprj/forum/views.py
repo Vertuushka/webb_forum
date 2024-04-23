c8d3e5944bb29314da8e8aaae4ec115ac26be0f1
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-04-23 11:36:05 +0200

    Forum_main page is done.
    v. 17

--- forumprj/forum/views.py
+++ forumprj/forum/views.py
@@ -4,22 +4,36 @@ from . models import *
 from users.models import Profile
 
 def forum_main(request):
-    nodes = Node.objects.all()
-    threads = Thread.objects.all()
-    tree = build_nodes_tree(nodes)
+    context = {}
+    if request.method == "POST":
+        context['forum_search'] = True
+        content_type = request.POST.get("content_type")
+        searched = request.POST.get("search")
+        if content_type:
+            if content_type == "threads":
+                results = Thread.objects.filter(title__icontains=searched)
+            if content_type == "members":
+                results = User.objects.filter(first_name__icontains=searched)
+        else:
+            results = Message.objects.filter(content__icontains=searched)
+        context['results'] = results
+    else:
+        nodes = Node.objects.all()
+        threads = Thread.objects.all()
+        tree = build_nodes_tree(nodes)
 
-    last_threads = []
-    for node in nodes:
-        thread = Thread.objects.filter(node=node, is_visible=True).order_by('-time_created')
-        if thread:
-            last_threads.append(thread[0])
+        last_threads = []
+        for node in nodes:
+            thread = Thread.objects.filter(node=node, is_visible=True).order_by('-time_created')
+            if thread:
+                last_threads.append(thread[0])
 
-    context = {
-        'tree':tree,
-        'threads':threads,
-        'last_threads': last_threads
-    }
-    # print(tree)
+        context = {
+            'tree':tree,
+            'threads':threads,
+            'last_threads': last_threads
+        }
+        # print(tree)
     return render(request, "forum_main.html", context)
 
 def build_nodes_tree(nodes, parent=None):
