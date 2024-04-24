fd0393980d40fbdc2f01b5ac817ab81570d8dcf5
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-04-24 11:40:06 +0200

    changing main page to forum

--- forumprj/forumprj/urls.py
+++ forumprj/forumprj/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from forum import views
 
 urlpatterns = [
+    path('', views.forum_main, name='mainpage'),
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('profile/', include('users.urls')),
