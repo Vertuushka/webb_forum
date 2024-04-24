b4d1d2c4e0005fcd3487f089b32021fb05f48697
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-04-24 13:51:44 +0200

    set threads sorting by last message time

--- forumprj/base/context_processors.py
+++ forumprj/base/context_processors.py
@@ -5,5 +5,5 @@ def menuProcessor(request):
     if request.user.is_authenticated:
         # profile = Profile.objects.get(user=user)
         avatar_path = settings.MEDIA_URL + str(request.user.first_name) 
-        return {'avatar_path': avatar_path, 'currentUser':request.user, "media_url": settings.MEDIA_URL,}
-    return {'avatar_path': None, "currentUser":None, "media_url": settings.MEDIA_URL,}
+        return {'avatar_path': avatar_path, "media_url": settings.MEDIA_URL,}
+    return {'avatar_path': None, "media_url": settings.MEDIA_URL,}
