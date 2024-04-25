2dc7110d5f8b990343adbb99f67b33ad408e086d
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-04-25 21:52:30 +0200

    moderaor's main page

--- forumprj/moderation/models.py
+++ forumprj/moderation/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from forum.models import Message
 from datetime import datetime
-# Create your models here.
-
-
+# Create your models here
 
 class Report(models.Model):
     reporter = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,8 +10,10 @@ class Report(models.Model):
     is_closed = models.BooleanField(default=False)
     is_declined = models.BooleanField(default=False)
     is_resolved = models.BooleanField(default=False)
-    assigned = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None, related_name="assigned")
-    time_created = models.DateTimeField(default=datetime.now())
+    assigned = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None, related_name="assigned", blank=True)
+    closed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None, related_name="closed_by", blank=True)
+    time_created = models.DateTimeField(default=None, null=True, blank=True)
+    time_changed = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.content.user} - {self.content.thread.title}'
--- forumprj/moderation/views.py
+++ forumprj/moderation/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def moderation_main(request):
-    pass
+    if request.user.has_perm('users.can_view_reports'):
+        active_reports = Report.objects.filter(is_closed=False).order_by('-time_created')
+        closed_reports = Report.objects.filter(is_closed=True).order_by('-time_created')[:50]
+        context = {
+            'active_reports':active_reports,
+            'closed_reports':closed_reports
+        }
+        return render(request, 'moderation_main.html', context)
+    else:
+        return render(request, 'error.html')
