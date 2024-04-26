766ba6d7fd0712e2415921bb83e084b1359ce369
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-04-26 22:00:06 +0200

    moderation_main

--- forumprj/moderation/models.py
+++ forumprj/moderation/models.py
@@ -7,15 +7,16 @@ from datetime import datetime
 class Report(models.Model):
     content = models.ForeignKey(Message, on_delete=models.SET_NULL, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    reason = models.TextField()
     is_closed = models.BooleanField(default=False)
     is_resolved = models.BooleanField(default=False)
-    is_declined = models.BooleanField(default=False)
+    is_rejected = models.BooleanField(default=False)
     assigned_to = models.ForeignKey(User, related_name="assigned_to", on_delete=models.SET_NULL, null=True, blank=True, default=None)
     closed_by = models.ForeignKey(User, related_name="closed_by", on_delete=models.SET_NULL, null=True, blank=True, default=None)
     is_notified = models.BooleanField(default=False)
-    notification = models.CharField(max_length=250)
-    time_changed = models.DateTimeField()
+    notification = models.CharField(max_length=250, blank=True, null=True)
+    time_changed = models.DateTimeField(null=True, blank=True)
     time_created = models.DateTimeField(default=datetime.now())
 
-    def __init__(self):
+    def __str__(self):
         return f'{self.content.user.username} - {self.content.thread.title}'
--- forumprj/moderation/views.py
+++ forumprj/moderation/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def moderation_main(request):
-    pass
+    try:
+        active_reports = Report.objects.filter(is_closed=False).order_by('-time_changed')
+    except:
+        active_reports = False
+    try:
+        closed_reports = Report.objects.filter(is_closed=True).order_by('-time_changed')
+    except:
+        closed_reports = False
+    context = {
+        "active_reports":active_reports,
+        "closed_reports":closed_reports
+    }
+    return render(request, 'moderation_main.html', context)
