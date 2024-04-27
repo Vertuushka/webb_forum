a1bd55be305e08c170b929cebe63a5882a681a40
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-04-27 19:42:15 +0200

    moderation is finally done

--- forumprj/moderation/views.py
+++ forumprj/moderation/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
-
+from django.contrib.auth.models import User, Permission
+from django.db.models import Q
+from datetime import datetime
 # Create your views here.
 def moderation_main(request):
     try:
@@ -20,7 +22,47 @@ def moderation_main(request):
 def report_details(request, id):
     try:
         report = Report.objects.get(id=id)
+        desired_permissions = Permission.objects.filter(codename='change_report')
+        users_with_permissions = User.objects.filter(
+            Q(user_permissions__in=desired_permissions) | 
+            Q(groups__permissions__in=desired_permissions) | 
+            Q(is_staff=True)
+        ).distinct()
     except:
         return render(request, 'error.html')
-    context = {"report":report}
+    if request.method == "POST":
+        if request.method == "POST":
+            status = request.POST.get('status')
+            assigned_to = request.POST.get('assigned_to')
+            is_notified = request.POST.get('is_notified', False)
+            if status != 'open':
+                if is_notified:
+                    # add code to send notification
+                    notification = request.POST.get('notification')
+                    report.notification = notification
+                    report.is_notified = True
+                else:
+                    report.is_notified = False
+                
+                closed_by = request.user
+                report.is_closed = True
+                report.is_rejected = (status == "reject")
+                report.is_resolved = not report.is_rejected
+                report.closed_by = closed_by
+                report.time_changed = datetime.now()
+                report.save()
+            else:
+                if assigned_to:
+                    try:
+                        report.assigned_to = User.objects.get(username=assigned_to)
+                    except:
+                        report.assigned_to = None
+                else:
+                    report.assigned_to = None
+                report.save()
+        return redirect('report_details', id)
+    context = {
+        "report":report,
+        "moderators": users_with_permissions
+        }
     return render(request, 'report_details.html', context)
