a588585bca89da4fd8cba2500402683c963b037b
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-04-29 20:23:54 +0200

    active reports + assigned reports amount

--- forumprj/base/context_processors.py
+++ forumprj/base/context_processors.py
@@ -1,9 +1,16 @@
 from django.conf import settings
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from moderation.models import Report
 
 def menuProcessor(request):
+    shared_data = {
+        "media_url": settings.MEDIA_URL
+    }
     if request.user.is_authenticated:
         avatar_path = settings.MEDIA_URL + str(request.user.profile.profile_picture) 
-        return {'avatar_path': avatar_path, "media_url": settings.MEDIA_URL,}
-    return {'avatar_path': None, "media_url": settings.MEDIA_URL,}
+        shared_data["avatar_path"] = avatar_path
+    if request.user.has_perm("moderation.view_report"):
+        active_reports_amount = Report.objects.filter(is_closed=False)
+        assigned_reports = active_reports_amount.filter(assigned_to=request.user) 
+        shared_data['reports_amount'] = len(active_reports_amount)
+        shared_data['assigned_reports'] = len(assigned_reports)
+    return shared_data
