bc2b80c8fe916e7833ae29ca0ec28f13e011305d
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-04-30 15:51:16 +0200

    private section functionality

--- forumprj/users/models.py
+++ forumprj/users/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+        
 class Warnings_history(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.ForeignKey(Message, on_delete=models.CASCADE)
