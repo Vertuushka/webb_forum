87f79aa885b861917d1a33afb3bcd23af0e0e631
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-05-15 08:58:26 +0200

    update permissionshandler, add warn func
    JS warn form
    warn handler (url + views + middleware)
    in warnings models alter foreignkey to OneToOneField

--- forumprj/base/static/script.js
+++ forumprj/base/static/script.js
@@ -5,20 +5,49 @@ $(document).ready(function() {
 
     onWindowResize();
     window.addEventListener('resize', onWindowResize);
-    var warnBtn = document.querySelectorAll(".warnBtn");
-    warnBtn.forEach(function(button){
-        button.onclick = function()
-        {
-            var id = this.parentElement.parentElement.parentElement.parentElement.getAttribute("id")
-            document.querySelector(".content").appendChild(CreateWarningForm(id, 0, GetCsrfToken()))
-        }
-    });
+
+    // warning btn
+    $(".warnBtn").each(function(index, button){
+        let msg = this.parentElement.parentElement.parentElement.lastElementChild.innerHTML;
+        let id = this.parentElement.parentElement.parentElement.parentElement.getAttribute("id");
+        this.addEventListener('click', function(){
+            console.log(msg)
+            $("body").append(`
+            <form action="/forum/message/${id}/warn/" method="post">
+                <input type="hidden" name="csrfmiddlewaretoken" value="${GetCsrfToken()}">
+                <input type="text" name="warn_reason" id="">
+                <label for=""><input type="checkbox" name="is_deleted" id="is_deleted">Delete message on warn</label>
+                <input type="text" name="deleting_reason" id="deleting_reason" readonly>
+                <input type="submit" value="Warn">
+            </form>
+            `)
+
+        $("#is_deleted").click(function(){
+            if(this.checked){
+                $("#deleting_reason").removeAttr("readonly")
+            }
+            else{$("#deleting_reason").attr("readonly", true) }
+        })
+        })
+    })
+    
+    // warnBtn.forEach(function(button){
+    //     button.click(function(){
+    //         var msg_text = this.parentElement.parentElement.parentElement.lastElementChild.innerHTML
+    //         var id = this.parentElement.parentElement.parentElement.parentElement.getAttribute("id")
+    //         document.querySelector(".content").appendChild(CreateWarningForm(id, msg_text, GetCsrfToken()))
+    //     })
+    // });
+
+    // edit btn
+
+    // delete btn
+
 });
 
 function GetCsrfToken()
 {
-    var token = document.querySelector("#token");
-    csrf = token.getAttribute("value");
+    csrf = $("#token").attr("value");
     return csrf
 }
 
