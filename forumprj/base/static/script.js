2c8d01180d651a921e79a0ad76d8d7d036d75dd6
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-05-16 19:50:16 +0200

    JS: deleting form + labels

--- forumprj/base/static/script.js
+++ forumprj/base/static/script.js
@@ -16,9 +16,11 @@ $(document).ready(function() {
             $("body").append(`
             <form action="/forum/message/${id}/warn/" method="post">
                 <input type="hidden" name="csrfmiddlewaretoken" value="${GetCsrfToken()}">
+                <label for="warn_reason">Warning reason:</label>
                 <input type="text" name="warn_reason" id="">
-                <label for=""><input type="checkbox" name="is_deleted" id="is_deleted">Delete message on warn</label>
-                <input type="text" name="deleting_reason" id="deleting_reason" readonly>
+                <label for=""><input type="checkbox" name="is_deleted" id="">Delete message on warn</label>
+                <label for="deleting_reason">Reason:</label>
+                <input type="text" name="deleting_reason" id="">
                 <input type="submit" value="Warn">
             </form>
             `)
@@ -40,10 +42,13 @@ $(document).ready(function() {
         let id = this.parentElement.parentElement.parentElement.parentElement.getAttribute("id");
         this.addEventListener('click', function(){
             $("body").append(`
-            <form action="" method="post">
+            <form action="/forum/message/${id}/delete/" method="post">
                 <input type="hidden" name="csrfmiddlewaretoken" value="${GetCsrfToken()}">
+                <label for="reason">Reason:</label>
                 <input type="text" name="reason" id="">
+                <label for="is_notified">Notify user</label>
                 <input type="checkbox" name="is_notified" id="">
+                <label for="notification">Notification:</label>
                 <input type="text" name="notification" id="">
                 <input type="submit" value="Delete">
             </form>
